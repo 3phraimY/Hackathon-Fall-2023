0d54b2e2fcccd25f24454ef66429899eeb065aeb
3phraimY <ephraim.young@cuw.edu>
2023-10-29 20:06:51 -0500

    Front and Back end

---
+++ backend/eventCreator.js
@@ -0,0 +1,21 @@
+const tools = require('nostr-tools')
+
+exports.createEvent = function(priv_key, kind, mssg) {
+    let event = {
+        kind: kind,
+        created_at: Math.floor(Date.now() / 1000),
+        tags: [],
+        content: mssg,
+        pubkey: tools.getPublicKey(priv_key),
+      }
+    
+    event.id = tools.getEventHash(event)
+    event.sig = tools.getSignature(event, priv_key)
+
+    let ok = tools.validateEvent(event)
+    let veryOk = tools.verifySignature(event)
+    
+    if(ok && veryOk) {
+        return event
+    }
+}
---
+++ backend/index.js
@@ -0,0 +1,34 @@
+// imports nostr-tools from the node_modules folder.
+// "tools" is the object through which we can interact with
+// nostr-tools' functions.
+const tools = require('nostr-tools')
+const eventCreator = require('./eventCreator')
+const express = require('express')
+const app = express()
+
+console.log("please throw")
+privKey = '3de315e757169d493073245ebb92d7053b3b608cb43f4ddcf43e8a40029ffbb1'
+
+app.get("/", function (req, res) {
+    res.sendFile("C:/Users/aschn/source/repos/CUWHackathon23/CUWHackathon23/frontend/index.html")
+})
+app.get("/index.html", function (req, res) {
+    res.sendFile("C:/Users/aschn/source/repos/CUWHackathon23/CUWHackathon23/frontend/index.html")
+})
+app.get("/chatting.html", function (req, res) {
+    res.sendFile("C:/Users/aschn/source/repos/CUWHackathon23/CUWHackathon23/frontend/chatting.html")
+})
+app.get("/faq.html", function (req, res) {
+    res.sendFile("C:/Users/aschn/source/repos/CUWHackathon23/CUWHackathon23/frontend/faq.html")
+})
+app.get("/sign-in.html", function (req, res) {
+    res.sendFile("C:/Users/aschn/source/repos/CUWHackathon23/CUWHackathon23/frontend/sign-in.html")
+})
+app.listen(3000, function () {
+    console.log("Server is running on localhost3000")
+})
+
+help = eventCreator.createEvent(privKey, 1, "hello.")
+
+console.log('event ID: ', help.id)
+console.log('event signature: ', help.sig)
